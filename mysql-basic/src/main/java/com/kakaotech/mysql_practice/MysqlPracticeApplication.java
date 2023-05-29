7e0e73eafacf05db3f70d8b3f64b7665cd8971e6
SeokjunMoon <seokjun0915@icloud.com>
2023-05-29 17:26:08 +0900

    feat: mysql 프로젝트 추가
    - mysql용 프로젝트 추가

---
+++ mysql-basic/src/main/java/com/kakaotech/mysql_practice/MysqlPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kakaotech.mysql_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlPracticeApplication.class, args);
+    }
+
+}
---
+++ mysql-basic/src/test/java/com/kakaotech/mysql_practice/MysqlPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaotech.mysql_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
